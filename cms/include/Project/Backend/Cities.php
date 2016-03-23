737cd28a43d5e392359fd6415dcee674e8e084d2
Jose on Vbox <jose.dsilva@bombayworks.se>
2016-03-23 21:41:01 +0530

    add Cities backend module for ModelView framework.

---
+++ cms/include/Project/Backend/Cities.php
@@ -0,0 +1,28 @@
+<?php
+
+class Project_Backend_Cities extends Curry_Backend
+{
+    /**
+     * Create a group icon in the Admin module panel
+     * and make this module available in the specified group.
+     * @return string
+     */
+    public static function getGroup()
+    {
+        return 'Demo ModelView';
+    }
+    
+    /**
+     * The method called by default by the cms.
+     * Something similar to C program - or rather the first function called.
+     */
+    public function showMain()
+    {
+        
+    }
+    
+    /*public function showDummyView()
+    {
+        echo 'Dummy view';
+    }*/
+}
