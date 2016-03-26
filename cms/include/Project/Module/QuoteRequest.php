2f042c70915f33eb08040b0401eb2e22330f757f
Jose F. D'Silva <josedsilva@yahoo.com>
2016-03-26 14:54:49 +0530

    Add QuoteRequest page module.

---
+++ cms/include/Project/Module/QuoteRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+class Project_Module_QuoteRequest extends Curry_Module
+{
+    
+    /* public function showFront(Curry_Twig_Template $template = null)
+    {
+        $r = $this->getRequest();
+        
+        // TODO: front end code
+        
+        if (null !== $template) {
+            return $template->render(array(
+                
+            ));
+        }
+    } */
+    
+    public function toTwig()
+    {
+        $r = $this->getRequest();
+        $isSaved = false;
+        
+        $form = new Curry_Form_ModelForm('Quote', array(
+            'withRelations' => array('RecyclingType', 'City'),
+            'columnElements' => array(
+                'relation__recyclingtype' => array('select', array(
+                    'multiOptions' => array(null => '[ Select ]') + RecyclingTypeQuery::create()
+                        ->orderByName()
+                        ->find()
+                        ->toKeyValue('PrimaryKey', 'Name'),
+                    'required' => true,
+                )),
+                'relation__city' => array('select', array(
+                    'multiOptions' => array(null => '[ Select ]') + CityQuery::create()
+                        ->orderByName()
+                        ->find()
+                        ->toKeyValue('PrimaryKey', 'Name'),
+                    'required' => true,
+                )),
+                'created_at' => false,
+                'updated_at' => false,
+                'status' => false,
+            ),
+        ));
+        $form->addElement('submit', 'save', array('label' => 'Submit'));
+        
+        if ($r->isPost() && $form->isValid($r->post)) {
+            $quote = new Quote();
+            $form->fillModel($quote);
+            $quote->save();
+            $isSaved = true;
+        }
+        return array(
+            'form' => $form,
+            'is_saved' => $isSaved,
+        );
+    }
+    
+    public function showBack()
+    {
+        return new Curry_Form_SubForm(array(
+            'elements' => array(
+                
+            ),
+        ));
+    }
+    
+    public function saveBack(Zend_Form_SubForm $form)
+    {
+        $values = $form->getValues(true);
+    }
+}
