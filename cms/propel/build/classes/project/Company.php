0edfb0ae7f779535e8fed2a25d740e9a857ace47
Jose F. D'Silva <josedsilva@yahoo.com>
2016-03-27 18:19:49 +0530

    email function for quote

--- cms/propel/build/classes/project/Company.php
+++ cms/propel/build/classes/project/Company.php
@@ -15,4 +15,32 @@
  */
 class Company extends BaseCompany
 {
+    const QUOTE_MAIL_TPL_ID = 8;
+    const BUY_QUOTE_PAGE_LINK = 'demo/buy-quote/';
+    
+    public function sendQuoteMail(Quote $quote, $subject = 'New quote request')
+    {
+        $mailTpl = PageQuery::create()->findPk(self::QUOTE_MAIL_TPL_ID);
+        if (!$mailTpl) {
+            throw new Exception('Cannot find mail template page.');
+        }
+        
+        $globals = array(
+            'company' => $this,
+            'quote' => $quote,
+            'buy_quote_link' => url(self::BUY_QUOTE_PAGE_LINK, array(
+                'qid' => $this->getPrimaryKey()
+            ))->getAbsolute('&', true),
+        );
+        
+        $mail = Curry_Mail::createFromPage($mailTpl, null, $globals);
+        $mail->addTo($this->getClientEmail(), $this->getClientName());
+        $mail->setFrom(Curry_Core::$config->curry->adminEmail, Curry_Core::$config->curry->projectName);
+        $mail->setSubject($subject);
+        try {
+            $mail->send();
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
 }
